a9f0fbed8875d33d525e255a3333b8dffbff0e56
Souvik <souvik.viksou@gmail.com>
2020-06-28 17:11:49 +0530

    Everything i changed...
    Added Nokia Homescreen...

---
+++ lib/NokiaScreens/Homescreen.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class Homescreen {
+  signal() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 20,
+                    width: 10,
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 6,
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 3.6,
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 3.6,
+                    decoration: BoxDecoration(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Image.asset('images/tower.jpg'),
+          ],
+        ),
+        lock()
+      ],
+    );
+  }
+
+  lock() {
+    return Container(
+      height: 20,
+      width: 40,
+      decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('images/lock.jpg'))),
+    );
+  }
+
+  center() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              'Jio',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nokia'),
+            ),
+            Text(
+              'Souvik',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nokia'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  batteryBar() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                height: 20,
+                width: 10,
+                decoration: BoxDecoration(color: Colors.black),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Container(
+                height: 20,
+                width: 6,
+                decoration: BoxDecoration(color: Colors.black),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Container(
+                height: 20,
+                width: 3.6,
+                decoration: BoxDecoration(color: Colors.black),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Container(
+                height: 20,
+                width: 3.6,
+                decoration: BoxDecoration(color: Colors.black),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Container(
+            height: 40,
+            width: 30,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/battery.jpg'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  time() {
+    var date = TimeOfDay.now();
+    print(date);
+    var fdate = date.toString().substring(11);
+    var odate = fdate.split(')');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text('${odate[0]}',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Nokia',
+              color: Colors.black,
+            )),
+      ],
+    );
+  }
+
+  homeScreen(height, width) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(color: Colors.green),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: signal(),
+              flex: 1,
+            ),
+            Expanded(
+              child: center(),
+              flex: 2,
+            ),
+            //SizedBox(width: 20),
+            Expanded(
+              child: time(),
+              flex: 1,
+            ),
+            Expanded(
+              child: batteryBar(),
+              flex: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
